8b642011f87bb7e7cace815a8d93a0ad8dc51760
MrPloman <polplanaayet@gmail.com>
2026-08-28 20:46:41 +0200

    Fix[IsPlatform implemented]: GetCartInMemory limited by isplatform

--- src/app/infrastructure/cart/memoryCart.repository.ts
+++ src/app/infrastructure/cart/memoryCart.repository.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { CartItem } from '../../core/domain/models/cartItem';
 import { Color } from '../../core/domain/models/color';
 import { Money } from '../../core/domain/models/money';
@@ -9,27 +10,36 @@ import { CartRepository } from '../../core/domain/ports/cart.repository';
 
 @Injectable({ providedIn: 'root' })
 export class InMemoryCartRepository implements CartRepository {
+  private platformId = inject(PLATFORM_ID);
+  private isBrowser = isPlatformBrowser(this.platformId);
   async load(): Promise<CartItem[]> {
-    const raw = localStorage.getItem('cart');
-    if (!raw) return [];
-    const parsed = JSON.parse(raw);
-    return parsed.map((plain: any) =>
-      CartItem.createCartItem(
-        ProductSku.createProductSku(plain.sku.value),
-        plain.productName,
-        Size.createSize(plain.size.system, plain.size.value),
-        Color.createColor(plain.color.value),
-        Money.createMoney(plain.priceAtAddTime.amountInCents, plain.priceAtAddTime.currency),
-        Quantity.createQuantity(plain.quantity.value),
-        plain.img,
-      ),
-    );
+    try {
+      if (!this.isBrowser) return [];
+      const raw = localStorage.getItem('cart');
+      if (!raw) return [];
+      const parsed = JSON.parse(raw);
+      return parsed.map((plain: any) =>
+        CartItem.createCartItem(
+          ProductSku.createProductSku(plain.sku.value),
+          plain.productName,
+          Size.createSize(plain.size.system, plain.size.value),
+          Color.createColor(plain.color.value),
+          Money.createMoney(plain.priceAtAddTime.amountInCents, plain.priceAtAddTime.currency),
+          Quantity.createQuantity(plain.quantity.value),
+          plain.img,
+        ),
+      );
+    } catch (error) {
+      return [];
+    }
   }
 
   async save(cart: CartItem[]): Promise<void> {
+    if (!this.isBrowser) return;
     await localStorage.setItem('cart', JSON.stringify(cart));
   }
   async clear(): Promise<void> {
+    if (!this.isBrowser) return;
     const cart = localStorage.getItem('cart');
     if (!cart) return;
     await localStorage.removeItem('cart');
